4f2d447546936b1ecbaf3ddce0cbcebd70e20d76
David Puerta Martín <puertadavid96182@gmail.com>
2023-10-31 00:59:28 -0400

    add env file for api

--- todoapi/todoapi/settings.py
+++ todoapi/todoapi/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#n)k@(%9c++8h$yxt35cd7s1mzxv0g8@t&k0d73#wca0ztc$g&'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = int(os.environ.get("DEBUG", default=False))
 
 ALLOWED_HOSTS = []
 
