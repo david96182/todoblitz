2d18a40fcf90ce066f17effa2705c0ae1e07ddf9
David Puerta Martín <puertadavid96182@gmail.com>
2023-10-31 16:11:06 -0400

    create api views

--- todoapi/tasks/views.py
+++ todoapi/tasks/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework.generics import ListAPIView, CreateAPIView, UpdateAPIView, DestroyAPIView
+from .models import Task
+from .serializers import TaskSerializer
 
-# Create your views here.
+
+class TaskListCreateView(ListAPIView, CreateAPIView):
+    """
+    List and Create task view
+    """
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+
+
+class TaskUpdateDeleteView(UpdateAPIView, DestroyAPIView):
+    """
+    Update and Delete task view
+    """
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer 
