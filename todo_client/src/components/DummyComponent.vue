a797855f7ab16ff2b16facfaf301c84eaaa6ac7d
David Puerta Martín <puertadavid96182@gmail.com>
2023-11-01 03:26:26 -0400

    avoid warning for having routes without component

---
+++ todo_client/src/components/DummyComponent.vue
@@ -0,0 +1,11 @@
+// DummyComponent.vue
+<template>
+  <div></div>
+  <!-- This component doesn't render anything -->
+</template>
+
+<script>
+export default {
+  name: 'DummyComponent'
+}
+</script>
--- todo_client/src/router.js
+++ todo_client/src/router.js
@@ -1,10 +1,11 @@
 // router.js
 import { createRouter, createWebHashHistory } from 'vue-router';
+import DummyComponent from './components/DummyComponent.vue';
 
 const routes = [
-  { path: '/all', name: 'all' },
-  { path: '/active', name: 'active' },
-  { path: '/completed', name: 'completed' },
+  { path: '/all', name: 'all', component: DummyComponent },
+  { path: '/active', name: 'active', component: DummyComponent },
+  { path: '/completed', name: 'completed', component: DummyComponent },
   { path: '/', redirect: '/all' },
 ];
 
